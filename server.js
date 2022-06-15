9ca01a71fd599180edca4d28aa54c1a25b43dbbf
mishiii06 <ubuntu@ip-172-31-91-72.ec2.internal>
2022-06-15 09:37:27 +0000

    final

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+
+app.use('/', (req, res) => {
+MongoClient.connect("mongodb://localhost:27017/test", function(err, db){
+db.collection('Example', function(err, collection){
+collection.insert({ pageHits: 'pageHits' });
+db.collection('Example').count(function(err, count){
+if(err) throw err;
+res.status(200).send('Page Hits: ' + Math.floor(count/2));
+});
+});
+});
+});
+
+app.listen(3000);
+console.log('Server running at http://localhost:3000/');
+
+module.exports = app;
